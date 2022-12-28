4c372951d0bfd0944897f14c8eb58ed7d7da875d
Shovan Nayak <77014833+shovannayak797@users.noreply.github.com>
2022-12-28 19:12:40 +0530

    Add files via upload

---
+++ addTwoNo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter 1st no");
+    scanf("%d",&a);
+    printf("enter 2nd no"); 
+    scanf("%d",&b); 
+    c=a+b; 
+    printf("sum is %d",c);
+
+    return 0;
+}
